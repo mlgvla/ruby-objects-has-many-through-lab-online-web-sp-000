aca3f40a4d459a1ba121bc7557cc899cd2418055
Monica Gerard <mlgerardvla@gmail.com>
2020-03-23 22:07:22 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -22,5 +22,10 @@ class Artist
     Song.new(name, self, genre)
   end
 
+  def genres
+    songs.collect do |song|
+      song.genre
+    end
+  end
 
 end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Genre
 
   attr_accessor :name
@@ -10,6 +11,18 @@ class Genre
 
   def self.all
     @@all
+  end
+
+  def songs
+    Song.all.collect do |song|
+      if song.genre.name == self.name
+        song
+      end
+    end
+  end
+
+  def artists
+    songs.collect {|song| song.artist}
   end
 
 end
